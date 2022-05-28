8ada171fc960af7893be0ac5eeb413873e2bc8c9
Nikolaj Bjorner <nbjorner@microsoft.com>
2022-05-28 11:05:46 -0700

    initial stab at guide by merge of virb3 and zucker
    Signed-off-by: Nikolaj Bjorner <nbjorner@microsoft.com>

--- README.md
+++ README.md
@@ -15,6 +15,21 @@ npm run build
 
 The built website will be in `dist/`.
 
+## Running locally
+
+```bash
+md z3-wasm
+xcopy dist z3-wasm
+npx parcel z3-wasm\index.html
+```
+
+Open a browser to http://localhost:1234
+
+
 ## Credits
 
 Front end heavily influenced from [cpitclaudel/z3.wasm](https://github.com/cpitclaudel/z3.wasm).
+
+* integration with released version of z3 forked from ViRb3, 
+
+* multiple examples based on Phillip Zucker's demo
--- src/main.js
+++ src/main.js
@@ -9,7 +9,9 @@ import initZ3 from "../node_modules/z3-solver/build/z3-built";
   var document = window.document;
   var command_line_args = document.getElementById("command-line-args");
   var run_button = document.getElementById("run");
-  var stdout_textbox = document.getElementById("stdout");
+    var stdout_textbox = document.getElementById("stdout");
+
+    var elapsed;
 
   function clear(node) {
     while (node.hasChildNodes()) {
@@ -18,22 +20,23 @@ import initZ3 from "../node_modules/z3-solver/build/z3-built";
   }
 
   async function verifyCurrentInput(_event) {
-    var input = editor.getValue();
-    clear(stdout_textbox);
-    disableButton("Running…");
-    verification_start = window.performance.now();
-    const cfg = Z3.mk_config();
-    const ctx = Z3.mk_context(cfg);
-    Z3.del_config(cfg);
-    var output = "";
-    for (const item of input.split("\n")) {
-      output = output.concat(await Z3.eval_smtlib2_string(ctx, item));
-    }
-    enableButton();
-    var elapsed = Math.round(window.performance.now() - verification_start);
-    logOutput(output, "stdout-msg");
-    logOutput("-- Verification complete (" + elapsed + "ms)", "info-msg");
-    Z3.del_context(ctx);
+      var input = editor.getValue();
+      	clear(stdout_textbox);
+	disableButton("Running…");
+	verification_start = window.performance.now();
+	const cfg = Z3.mk_config();
+	const ctx = Z3.mk_context(cfg);
+	Z3.del_config(cfg);
+	var output = "";
+	for (const item of input.split("\n")) {
+	    output = output.concat(await Z3.eval_smtlib2_string(ctx, item));
+	}
+	enableButton();
+	elapsed = Math.round(window.performance.now() - verification_start);
+	Z3.del_context(ctx);
+
+      logOutput(output, "stdout-msg");
+      logOutput("-- Verification complete (" + elapsed + "ms)", "info-msg");
   }
 
   function disableButton(message) {
@@ -82,15 +85,66 @@ import initZ3 from "../node_modules/z3-solver/build/z3-built";
 (exit)`);
   }
 
-  setupAceEditor();
-  clear(stdout_textbox);
 
-  const mod = async () =>
-    await initZ3({
-      locateFile: (f) => f,
-      mainScriptUrlOrBlob: "z3-built.js",
-    });
-  const { em, Z3 } = await init(mod);
-  enableButton();
-  run_button.onclick = verifyCurrentInput;
+    // setupAceEditor();
+    // clear(stdout_textbox);
+
+    const mod = async () =>
+	  await initZ3({
+	      locateFile: (f) => f,
+	      mainScriptUrlOrBlob: "z3-built.js",
+	  });
+    const { em, Z3 } = await init(mod);
+    // enableButton();
+
+    // code from Zucker demo
+    // far from as structured, but for testing multi-entry version
+    
+    var examples = document.getElementsByTagName("pre");
+    console.log(examples)
+    examples = Array.from(examples)
+    for (let code of examples) {
+        if (code.className == "listing") {
+	    let div = document.createElement("div");
+	    
+	    let ta = document.createElement("textarea");
+	    let result = document.createElement("code");
+	    result.style.whiteSpace = "pre-wrap";
+	    
+	    let button = document.createElement("button");
+	    let br = document.createElement("br");
+	    
+	    ta.style.width = "100%";
+	    ta.innerHTML = code.textContent.replace(/\r?\n/g, '\r\n');
+	    button.innerText = "Run"
+	    button.onclick = async () => {
+                try {
+		    // clear(stdout_textbox);
+		    // disableButton("Running…");
+		    verification_start = window.performance.now();
+		    const cfg = Z3.mk_config();
+		    const ctx = Z3.mk_context(cfg);
+		    Z3.del_config(cfg);
+		    var output = "";
+		    for (const item of ta.value.split("\n")) {
+			output = output.concat(await Z3.eval_smtlib2_string(ctx, item));
+		    }
+		    elapsed = Math.round(window.performance.now() - verification_start);
+		    Z3.del_context(ctx);
+		    console.log(output)
+		    result.innerText = output;
+                } catch (error) {
+		    console.error(error);
+		    result.innerText = "Error. See Javascript console for more detail";
+                }
+	    }
+	    div.appendChild(ta);
+	    div.appendChild(button);
+	    div.appendChild(br);
+	    div.appendChild(result);
+	    code.parentNode.replaceChild(div, code);
+        }
+    }
+
+    // run_button.onclick = verifyCurrentInput;
 })();
